efe32c782917565708348447dd5eb20b27685460
Unknown <hemaxi.1996@gmail.com>
2022-07-29 11:28:27 +0100

    step size

--- predict_main.py
+++ predict_main.py
@@ -5,7 +5,7 @@ img_dir = r'/mnt/2TBData/hemaxi/Downloads/percentile'
 save_dir_2dmasks = r'/mnt/2TBData/hemaxi/cycleGAN/26_05_2022/models_02_07/results2'
 save_dir_3dmasks = r'/mnt/2TBData/hemaxi/cycleGAN/26_05_2022/models_02_07/masks2'
 mode_ = 'test'
-_step = 128
+_step = 64
 patch_size = (64,128,128,1)
 
 predict(model_dir, img_dir, save_dir_2dmasks, save_dir_3dmasks, mode_,  patch_size, _step)
