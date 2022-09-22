1347c9e43120b2c42b9a772cd22ed4bbf7c67acb
Hemaxi Narotamo <hemaxi.narotamo@tecnico.ulisboa.pt>
2022-09-22 19:16:18 +0100

    percentile normalization

--- preprocessing/percentile.py
+++ preprocessing/percentile.py
@@ -1,11 +1,11 @@
-#Percentile Equalization in 3D
-
+#Percentile Normalization in 3D
+#https://homepages.inf.ed.ac.uk/rbf/HIPR2/stretch.htm
 import tifffile
 import numpy as np
 import os
 
 images_dir = r'/mnt/2TBData/hemaxi/Downloads/tif' #original images
-save_dir = r'/mnt/2TBData/hemaxi/Downloads/percentile' #images after percentile equalization
+save_dir = r'/mnt/2TBData/hemaxi/Downloads/percentile' #images after percentile normalization
 
 for img_name in os.listdir(images_dir): 
 	mip_img = tifffile.imread(os.path.join(images_dir, img_name))
@@ -15,6 +15,4 @@ for img_name in os.listdir(images_dir):
 	maxval = np.percentile(mip_img, 99)
 	mip_img = np.clip(mip_img, minval, maxval)
 	mip_img = (((mip_img - minval) / (maxval - minval)) * 255).astype('uint8')	
-
-	#tifffile.imwrite(r'/mnt/2TBData/hemaxi/Downloads/aa.tif', np.max(mip_img, axis=2))
 	tifffile.imwrite(os.path.join(save_dir, img_name), mip_img)
