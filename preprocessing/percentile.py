e5077e3a4f6c2a5fd6b698486e065b46ba271f01
Hemaxi Narotamo <hemaxi.narotamo@tecnico.ulisboa.pt>
2023-09-29 16:03:33 +0100

    1 channel

--- preprocessing/percentile.py
+++ preprocessing/percentile.py
@@ -4,12 +4,11 @@ import tifffile
 import numpy as np
 import os
 
-images_dir = r'/mnt/2TBData/hemaxi/Downloads/tif' #original images
-save_dir = r'/mnt/2TBData/hemaxi/Downloads/percentile' #images after percentile normalization
+images_dir = r'C:\Users\hemax\Desktop\testing\images' #original images
+save_dir = r'C:\Users\hemax\Desktop\testing\perc' #images after percentile normalization
 
 for img_name in os.listdir(images_dir): 
 	mip_img = tifffile.imread(os.path.join(images_dir, img_name))
-	mip_img = mip_img[:,:,:,2] #blue channel (vessels channel)
 	print(np.shape(mip_img))
 	minval = np.percentile(mip_img, 1) 
 	maxval = np.percentile(mip_img, 99)
