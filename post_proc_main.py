6cc44d65794c251e09399f2f849c8cfeedfad2ad
Hemaxi Narotamo <hemaxi.narotamo@tecnico.ulisboa.pt>
2024-02-15 13:58:09 +0000

    updated post_proc 15/02

--- post_proc_main.py
+++ post_proc_main.py
@@ -1,9 +1,8 @@
 from utils.post_processing import *
 
-msk_dir = r'/mnt/2TBData/hemaxi/cycleGAN/26_05_2022/models_01_07/masks'  #3D masks (CycleGAN)
+msk_dir = r'/mnt/2TBData/hemaxi/cycleGAN/26_05_2022/models_01_07/masks'  #3D masks (CycleGAN)
 save_dir_masks2d = r'/mnt/2TBData/hemaxi/cycleGAN/26_05_2022/models_01_07/results_proc'
 save_dir_masks3d = r'/mnt/2TBData/hemaxi/cycleGAN/26_05_2022/models_01_07/masks_proc'
+resolution_file = r'/mnt/2TBData/hemaxi/Downloads/resolution.xlsx'
 
-mode_ = 'test'
-
-post_processing(msk_dir, save_dir_masks3d, save_dir_masks2d, mode_)
+post_processing(msk_dir, save_dir_masks3d, save_dir_masks2d, resolution_file)
