a393b79398c004c4ff0732470169abbb265f7d6f
Hemaxi Narotamo <hemaxi.narotamo@tecnico.ulisboa.pt>
2024-08-29 10:53:57 +0100

    Update predict_main.py
    - The resolution file is now required as input for the prediction model. This file is used to obtain percentile equalization values (pre-processing step) and to resize images if the _resize flag is True.
    - Added a norm_ parameter (True or False) to control whether percentile equalization is applied. When set to True, percentile equalization parameters will be automatically extracted from the resolution file.
    - Added a _resize parameter (True or False) to determine if images should be resized to match the resolution of the training images.
    - Implemented a progress bar to monitor the prediction process.
    - Image channels are now automatically transposed so that the output dimensions are (X, Y, Z).

--- predict_main.py
+++ predict_main.py
@@ -4,8 +4,12 @@ model_dir = '/mnt/2TBData/hemaxi/cycleGAN/26_05_2022/models_02_07/1st/g_model_At
 img_dir = r'/mnt/2TBData/hemaxi/Downloads/percentile'
 save_dir_2dmasks = r'/mnt/2TBData/hemaxi/cycleGAN/26_05_2022/models_02_07/results2'
 save_dir_3dmasks = r'/mnt/2TBData/hemaxi/cycleGAN/26_05_2022/models_02_07/masks2'
+path_res = r'/mnt/2TBData/hemaxi/Downloads/resolution.xlsx'
 mode_ = 'test'
 _step = 64
+norm_ = False #percentile based normalization
+_resize = False #image resizing to match the resolution of the training images
 patch_size = (64,128,128,1)
 
-predict(model_dir, img_dir, save_dir_2dmasks, save_dir_3dmasks, mode_,  patch_size, _step)
+predict(model_dir, img_dir, save_dir_2dmasks, save_dir_3dmasks, path_res, _resize, norm_, mode_,  patch_size, _step)
+
